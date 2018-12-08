f5c9b2c36ea9c80f47baa7325af1d076fb24c8ad
iremn <iremndemirtas@gmail.com>
2018-12-08 23:24:29 +0300

    main loop modified

--- code/rpc_client.py
+++ code/rpc_client.py
@@ -1,9 +1,9 @@
-from xmlrpc.client import ServerProxy
+from xmlrpc.client import ServerProxy, Binary
 import base64
 import bcrypt
 from config import name_server_url
 import argparse
-
+import os
 
 def sign_up(username, password):
     if len(password) > 72:
@@ -50,6 +50,11 @@ def list_file_names(user_id, cloud_file_path):
 
     return file_list
 
+def get_file_binary(local_path):
+    with open(local_path, 'rb') as file:
+        file_bin = Binary(file.read())
+    return file_bin
+
 
 class App(object):
     def __init__(self, user_id):
@@ -57,6 +62,41 @@ class App(object):
 
     def main_loop(self):
         list_file_names(self.user_id, '')
+        print('OPTIONS\n'
+              '- upload <file-path>      <cloud-path-to-upload>\n'
+              '- delete <path-of-file>\n'
+              '- fetch  <path-on-cloud>  <local-path-to-save>\n')
+
+        while True:
+            command = str(input('$ ')).split(' ')
+
+            if command[0] == 'upload' and len(command) == 3:
+                file_path = command[1]
+                cloud_file_path = command[1]
+                file_bin = get_file_binary(file_path)
+                proxy.upload_file(file_bin, cloud_file_path)
+
+            elif command[0] == 'delete' and len(command) == 2:
+                cloud_file_path = command[1]
+                proxy.delete_file(self.user_id, cloud_file_path)
+
+            elif command[0] == 'fetch' and len(command) == 3:
+                cloud_file_path = command[1]
+                local_path_to_save = command[2]
+                flag, file_bin = proxy.fetch_file(self.user_id, cloud_file_path)
+                path, filename = os.path.split(cloud_file_path)
+
+                if flag:
+                    with open(local_path_to_save + filename, "wb") as handle:
+                        decrypted = decrypt_file(file_bin)
+                        try:
+                            handle.write(decrypted)
+                            print('Saved ' + filename + 'to ' + local_path_to_save)
+                        except:
+                            print('Could not save ' + filename)
+            else:
+               print('INVALID COMMAND')
+
 
 
 if __name__ == '__main__':
